760a2064ecec93c92d5cbdc093850ffe12324392
Maciej Koczot <mefiuea@gmail.com>
2022-06-14 15:29:15 +0200

    install pymongo

--- app/main.py
+++ app/main.py
@@ -1,11 +1,29 @@
 from flask import Flask
+import pymongo
+from pymongo import errors
 
 app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host='db_mongo',
+        port=27018,
+        serverSelectionTimeoutMS=1000
+    )
+    mongo.server_info()
+except errors.ServerSelectionTimeoutError as err:
+    print(err)
+    print('PROBLEM')
 
 
 @app.route('/')
 def index():
     return 'Hello'
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    return 'X'
 
 
 if __name__ == '__main__':
